54b674bc39579bbbf4ad19babe413dc58d4c23eb
Lucas <fradkinlucas@hotmail.com>
2024-02-27 15:07:00 -0300

    Expandir e2e test

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -5,5 +5,11 @@ describe('Pokedex', function() {
       cy.contains('ivysaur')
       cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('pokemon page can be navigate to', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
+  })
 })
 /* eslint-disable */
